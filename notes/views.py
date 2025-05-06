ebcba8c7f079ede7e6f4fb4f0817eb3bce517b1e
sugam12 <sugamachr@gmail.com>
2025-05-06 17:45:15 +0800

    Authentication

--- notes/views.py
+++ notes/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from django.views.generic.edit import DeleteView
 from .forms import NotesForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 from .models import Notes
@@ -32,10 +33,20 @@ class NoteCreateView(CreateView):
     success_url = '/smart/notes'
     form_class = NotesForm
 
-class NoteListView(ListView):
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+class NoteListView(LoginRequiredMixin, ListView):
     model = Notes
     context_object_name = 'notes'
     template_name = "notes/notes_list.html"
+    login_url = "/admin"
+
+    def get_queryset(self):
+        return self.request.user.notes.all()
 
 class PopularNotesListView(ListView):
     model = Notes
